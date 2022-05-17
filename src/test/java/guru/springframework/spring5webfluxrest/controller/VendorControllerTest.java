9ded489e8b44cd6ec49de42bb9e13866b1b6fe37
shing <chishing@netvigator.com>
2022-05-17 21:01:56 +0800

    Initial commit for adding list and getById controller method for Vendor.  Test for list() not work.

---
+++ src/test/java/guru/springframework/spring5webfluxrest/controller/VendorControllerTest.java
@@ -0,0 +1,57 @@
+package guru.springframework.spring5webfluxrest.controller;
+
+import guru.springframework.spring5webfluxrest.domain.Category;
+import guru.springframework.spring5webfluxrest.domain.Vendor;
+import guru.springframework.spring5webfluxrest.repositories.VendorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+class VendorControllerTest {
+    private AutoCloseable closeable;
+
+    WebTestClient webTestClient;
+    @Mock
+    VendorRepository mockVendorRepository;
+    VendorController vendorController;
+
+    @BeforeEach
+    void setUp() {
+        closeable = MockitoAnnotations.openMocks(this);
+
+        //mockVendorRepository = Mockito.mock(VendorRepository.class);
+        vendorController = new VendorController(mockVendorRepository);
+        webTestClient = WebTestClient.bindToController(vendorController).build();
+
+    }
+
+    @Test
+    void list() {
+        BDDMockito.given(mockVendorRepository.findAll())
+                .willReturn(Flux.just(Vendor.builder().firstname("first name 1").lastname("last name 1").build(),
+                                      Vendor.builder().firstname("first name 2").lastname("last name 2").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendor/")
+                .exchange()
+                .expectBodyList(Vendor.class)
+                .hasSize(2);
+    }
+
+    @Test
+    void getById() {
+        BDDMockito.given(mockVendorRepository.findById("someid"))
+                .willReturn(Mono.just(Vendor.builder().firstname("first name").lastname("last name").build()));
+
+        webTestClient.get()
+                .uri("/api/v1/vendor/someid")
+                .exchange()
+                .expectBody(Category.class);
+
+    }
+}
